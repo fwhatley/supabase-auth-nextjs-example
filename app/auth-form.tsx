2ff87ef4ff8a44d733f7b81dc734cdff8fafa1a8
Fredy Whatley (fwhatley) <fwhatley@tradestation.com>
2024-01-28 02:22:48 -0500

    add auth callback url

--- app/auth-form.tsx
+++ app/auth-form.tsx
@@ -15,7 +15,7 @@ export default function AuthForm() {
       theme="dark"
       showLinks={false}
       providers={[]}
-      redirectTo="http://localhost:3000/auth/callback"
+      redirectTo={window.location.protocol + "//" + window.location.host + "/auth/callback"}
     />
   )
 }
