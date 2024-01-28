122c9cc57fc4a3664ad4d72743ebc7208d1df2b3
Fredy Whatley (fwhatley) <fwhatley@tradestation.com>
2024-01-28 02:47:06 -0500

    add redirect url

--- app/auth-form.tsx
+++ app/auth-form.tsx
@@ -7,12 +7,25 @@ import { Database } from './database.types'
 export default function AuthForm() {
   const supabase = createClientComponentClient<Database>()
 
-  const getRedirectUrl = () => {
-    if (window) {
-      return window.location.protocol + "//" + window.location.host + "/auth/callback"
-    }
-    return 'https://supabase-auth-nextjs-example-ykzk.vercel.app/auth/callback'
+  const getURL = () => {
+    let url =
+        process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+        process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+        'http://localhost:3000'
+    // Make sure to include `https://` when not localhost.
+    url = url.includes('http') ? url : `https://${url}`
+    // Make sure to include a trailing `/`.
+    url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
+    return url
   }
+
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: 'github',
+    options: {
+      redirectTo: getURL(),
+    },
+  })
+
 
   return (
     <Auth
@@ -22,7 +35,7 @@ export default function AuthForm() {
       theme="dark"
       showLinks={false}
       providers={[]}
-      // redirectTo={getRedirectUrl()}
+      redirectTo={getURL() + '/auth/callback'}
     />
   )
 }
